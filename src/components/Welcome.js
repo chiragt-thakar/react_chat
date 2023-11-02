6cb739f6895b03bac30429aecc06bbcb395aedb3
Chirag <chiragthakar066@gmail.com>
2023-11-03 00:16:55 +0530

    chat is working

---
+++ src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <>
+      <div className="col-6">
+            <div className="chat">
+            <h1>This is the welcome page</h1>
+            </div>
+          </div>
+    </>
+  )
+}
+
+export default Welcome
